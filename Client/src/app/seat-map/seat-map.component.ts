7e2018e878413470a4b50d281f83bbbcf05d7e6c
Vignesh C <poppyseed6464@gmail.com>
2025-07-11 00:23:00 +0530

    - added Bulk Booking modal - Bulk reservation

--- Client/src/app/seat-map/seat-map.component.ts
+++ Client/src/app/seat-map/seat-map.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { Seat } from '../Models/seat';
+import { NotificationService } from '../Service/notification.service';
 
 @Component({
   selector: 'app-seat-map',
@@ -7,10 +8,22 @@ import { Seat } from '../Models/seat';
   styleUrls: ['./seat-map.component.css']
 })
 export class SeatMapComponent implements OnChanges {
+
+  constructor(private notify:NotificationService){
+
+  }
   @Input() seatRows: Seat[] = []; // ⬅️ Data from parent
   @Output() seatSelected = new EventEmitter<Seat>(); // ⬅️ Send back selected seat
 
+// New Inputs for bulk booking
+  @Input() isSupervisor: boolean = false;
+  @Input() teamMembers: { id: string; name: string; selected?: boolean }[] = [];
+  @Output() bulkBooked = new EventEmitter<{ employeeId: string; seatId: string }[]>();
+
   chunkedSeatRows: Seat[][] = [];
+
+ showPopup = false;
+  
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['seatRows']) {
@@ -33,4 +46,34 @@ export class SeatMapComponent implements OnChanges {
     if (seat.status === 'booked') return;
     this.seatSelected.emit(seat);
   }
+
+//  Bulk Booking: Show team popup
+  openBulkBookingPopup() {
+    this.showPopup = true;
+  }
+
+  closePopup() {
+    this.showPopup = false;
+  }
+
+  // Bulk Booking logic
+  confirmBulkBooking() {
+    const selectedMembers = this.teamMembers.filter(emp => emp.selected);
+    const availableSeats = this.seatRows.filter(seat => seat.status === 'available');
+
+    if (selectedMembers.length > availableSeats.length) {
+      this.notify.showWarning(' Not enough seats available!')
+     // alert(' Not enough seats available!');
+      return;
+    }
+
+    const bookings = selectedMembers.map((member, index) => ({
+      employeeId: member.id,
+      seatId: availableSeats[index].id,
+    }));
+
+    this.bulkBooked.emit(bookings);
+    this.closePopup();
+
+}
 }
