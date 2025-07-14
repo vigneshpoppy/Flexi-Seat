2a38c1a723b64ed42d50adc2a795f8b2701f5862
Arun R <arun.raa1997@gmail.com>
2025-07-15 01:59:33 +0530

    Updated email service

---
+++ Core/FlexiSeat/FlexiSeat/DTO/EmailDTOs/TicketEmailDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FlexiSeat.DTO.EmailDTOs
+{
+    public class TicketEmailDTO
+    {
+        public string Id { get; set; }
+        public string Body { get; set; }
+        public string ADID { get; set; }
+
+    }
+}
